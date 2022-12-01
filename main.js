0d6a6ae0b8f73f35c5970b69687b2a4f33e7f320
Jennifer Techel <jennifertechel@hotmail.com>
2022-12-01 23:17:50 +0100

    Added js function for mobile menu

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-window.addEventListener("DOMContentLoaded", main);
+window.addEventListener('DOMContentLoaded', main);
 
 function main() {
-    
+  addEventListeners();
+  // call other function in future....
+}
+
+function addEventListeners() {
+  const burgerMenuButton = document.getElementById('open-button');
+  burgerMenuButton.onclick = toggleMenu;
+  // burgerMenuButton.addEventListener('click', toggleMenu);
+}
+
+function toggleMenu() {
+  const menu = document.querySelector("#mobile-menu");
+  menu.classList.toggle("show");
 }
