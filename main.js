4b6687396c350e0d684fc30ae11b5e477ede7a07
Jennifer Techel <jennifertechel@hotmail.com>
2022-12-02 12:41:55 +0100

    Added jsdoc

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
+/** Event that waits for HTML document to load before executing main function */
 window.addEventListener('DOMContentLoaded', main);
 
+/** Calling on another function */
 function main() {
-  addEventListeners();
-  // call other function in future....
+    addEvents();
 }
 
-function addEventListeners() {
-  const burgerMenuButton = document.getElementById('open-button');
-  burgerMenuButton.onclick = toggleMenu;
-  // burgerMenuButton.addEventListener('click', toggleMenu);
+/** Function that adds click event that calls on next function */
+function addEvents() {
+    /** @type {HTMLButtonElement} getting button from document */
+    const menuButton = document.getElementById('open-button');
+    menuButton.onclick = toggleMenu;
 }
 
+/** Function that toggles class that hides and displays menu */
 function toggleMenu() {
-  const menu = document.querySelector("#mobile-menu");
-  menu.classList.toggle("show");
+    /** @type {HTMLDivElement} getting hidden from document */
+    const menu = document.querySelector("#mobile-menu");
+    menu.classList.toggle("hide");
 }
+
+let options = {
+    root: document.querySelector('#scrollArea'),
+    rootMargin: '0px',
+    threshold: 1.0
+  }
+  
+  let observer = new IntersectionObserver(callback, options);
+
